209e63dd64a217d46100236b35816a055f8382cb
Keaton Burleson <keaton.burleson@me.com>
2019-05-13 16:02:51 -0500

    Initial Commit

---
+++ Install Wrapper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Install Wrapper
+//
+//  Created by Keaton Burleson on 5/13/19.
+//  Copyright © 2019 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
